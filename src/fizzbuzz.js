6cf6fb756828b1ad3e705f97cfc05fc0cb082fe1
Ariel Conti <ariel.agostinho@fcamara.com.br>
2020-06-27 19:28:23 -0300

    Implementadas as funções fizzBuzz e jsStripProperty

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,5 +1,29 @@
 const generateFizzBuzz = n => {
   // implemente seu código aqui
+
+  if (typeof n === 'string') {
+    return 'erro';
+  }
+
+  const response = [];
+
+  for (var i = 1; i <= n; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      response.push('FizzBuzz');
+    }
+    else if (i % 3 == 0) {
+      response.push('Fizz');
+    }
+    else if (i % 5 == 0) {
+      response.push('Buzz');
+    }
+    else {
+      response.push(i);
+    }
+  }
+  return response;
+
 };
+
 
 module.exports = generateFizzBuzz;
--- src/js-strip-property.js
+++ src/js-strip-property.js
@@ -1,5 +1,13 @@
 const stripProperty = (obj, prop) => {
   // Implemente seu código aqui
+
+  Object.entries(obj).map(p => {
+    if (prop == p[0]) {
+      delete obj[prop];
+    }
+  })
+
+  return obj;
 };
 
 module.exports = stripProperty;
