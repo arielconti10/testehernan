8d2c1b76be1fea7396e4103b78180bd0a18c69b2
Ariel Conti <ariel.agostinho@fcamara.com.br>
2020-06-29 16:32:03 -0300

    comentários adicionados para explicação

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,29 +1,35 @@
-const generateFizzBuzz = n => {
+const generateFizzBuzz = (n) => {
   // implemente seu código aqui
 
-  if (typeof n === 'string') {
-    return 'erro';
+  // Verificação de tipo da variavel recebida, se for string irá retornar erro
+  if (typeof n === "string") {
+    return "erro";
   }
 
+  // Inicia vetor de resposta
   const response = [];
 
+  // Laço para percorrer o número inputado
   for (var i = 1; i <= n; i++) {
+    /**
+     * Verificações e substituições dos valores, se for divisivel por 3 e 5, retornará FizzBuzz
+     * Se for divisivel por 3 e 5, retorna FizzBuzz
+     * Se for divisivel por 3, retorna Fizz,
+     * Se for divisivel por 5, retorna Buzz.
+     */
     if (i % 3 == 0 && i % 5 == 0) {
-      response.push('FizzBuzz');
-    }
-    else if (i % 3 == 0) {
-      response.push('Fizz');
-    }
-    else if (i % 5 == 0) {
-      response.push('Buzz');
-    }
-    else {
+      response.push("FizzBuzz");
+    } else if (i % 3 == 0) {
+      response.push("Fizz");
+    } else if (i % 5 == 0) {
+      response.push("Buzz");
+    } else {
       response.push(i);
     }
   }
+
+  // Retorna o vetor de resposta com as devidas substituições
   return response;
-
 };
-
 
 module.exports = generateFizzBuzz;
--- src/js-strip-property.js
+++ src/js-strip-property.js
@@ -1,12 +1,16 @@
 const stripProperty = (obj, prop) => {
-  // Implemente seu código aqui
-
-  Object.entries(obj).map(p => {
+  /**
+   * Laço para mapear o objeto como array
+   */
+  Object.entries(obj).map((p) => {
+    // Verificar se a propriedade inputada bate com alguma propriedade do objeto
     if (prop == p[0]) {
+      // Remove a proriedade solicitada do objeto
       delete obj[prop];
     }
-  })
+  });
 
+  // Retorna o objeto com as propriedades excluidas
   return obj;
 };
 
